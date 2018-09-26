408d20a305a3db5758cb4d4d99f2406e33256740
Byung Joo Kim <byungkim82@gmail.com>
2018-09-25 21:55:31 -0400

    added second pickups

---
+++ Assets/Assignment6/Scripts/Spaceship_movement.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spaceship_movement : MonoBehaviour {
+    public float floatStrength;
+    private float originalY;
+	// Use this for initialization
+	void Start () {
+        originalY = transform.position.y;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x, originalY + (float)Mathf.Sin(Time.time) * floatStrength, transform.position.z);
+	}
+}
